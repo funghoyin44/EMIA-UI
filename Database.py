e6a2aa43509dec702fa3c87b1b22cfb02d56f278
H.Y. FUNG <hyfungak@connect.ust.hk>
2023-04-27 02:06:08 +0800

    Update Database.py

--- Database.py
+++ Database.py
@@ -3,7 +3,7 @@ class Homework:
         self.course = course
         self.homework = homework
         self.time = time
-
+        homework_list.append(self)
 
     def get_course(self)->str:
         return self.course
@@ -13,9 +13,32 @@ class Homework:
     
     def get_time(self)->int:
         return self.time
+
+class User:
+    def __init__(self, username:str, password:str, first_name:str, last_name:str):
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        user_list.append(self)
+
+    def get_username(self)->str:
+        return self.username
+    
+    def check_password(self, password)->bool:
+        if password == self.password:
+            return True
+        return False
+
+    def get_first_name(self)->str:
+        return self.first_name
+    
+    def get_last_name(self)->str:
+        return self.last_name
     
 course_list = []
 homework_list = []
+user_list = []
 
 def initialize_data():
     PHYS3032_PS1 = Homework("PHYS3032", "PS1", 130)
@@ -26,14 +49,12 @@ def initialize_data():
     PHYS3032_PS6 = Homework("PHYS3032", "PS6", 210)
     PHYS3032_PS7 = Homework("PHYS3032", "PS7", 230)
     PHYS3032_PS8 = Homework("PHYS3032", "PS8", 140)
-    homework_list.append(PHYS3032_PS1)
-    homework_list.append(PHYS3032_PS2)
-    homework_list.append(PHYS3032_PS3)
-    homework_list.append(PHYS3032_PS4)
-    homework_list.append(PHYS3032_PS5)
-    homework_list.append(PHYS3032_PS6)
-    homework_list.append(PHYS3032_PS7)
-    homework_list.append(PHYS3032_PS8)
     course_list.append("PHYS3032")
+    EMIA_HW1 = Homework("EMIA2020", "HW1", 120)
+    EMIA_HW2 = Homework("EMIA2020", "HW2", 130)
+    EMIA_HW3 = Homework("EMIA2020", "HW3", 90)
+    course_list.append("EMIA2020")
+    user0 = User("andrew", "123456", "Andrew", "Ho")
+    user1 = User("mary3308", "qwer789", "Megumi", "Kato")
 
 initialize_data()
